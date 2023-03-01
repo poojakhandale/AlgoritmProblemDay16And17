bf6e29d6301b9cb9f31c7f4e72eca810063a8d1c
poojakhandale <poojakhandale2000@gmail.com>
2023-03-01 21:11:16 +0530

    Check for anagrams

--- Program.cs
+++ Program.cs
@@ -6,17 +6,10 @@ class Program
     public static void Main(string[] args)
     {
         {
-            {
-                int[] arr = { 52, 4, 300, 21, 102 };
-
-                Console.WriteLine("Array before sorting:");
-                Sorting.PrintArray(arr);
-
-                Sorting.MergeSort(arr, 0, arr.Length - 1);
-
-                Console.WriteLine("\nArray after sorting:");
-                Sorting.PrintArray(arr);
+          
+                Sorting ana = new Sorting();
+                ana.Display();
             }
         }
     }
-}
+
