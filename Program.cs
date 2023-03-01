29f487098b580f338ef160e2da8372d93aef8f49
poojakhandale <poojakhandale2000@gmail.com>
2023-03-01 19:45:25 +0530

    Sort the array using Insertion sort

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmProblem;
+
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine( "Welcome to the alogithm problems");
+        {
+            Sorting sort = new Sorting();
+            sort.Display();
+        }
     }
 }
